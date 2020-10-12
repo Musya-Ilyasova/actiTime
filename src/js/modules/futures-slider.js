17cdd51ef6eab15006f93aede4d4fcf8b90a6d6e
Musya <abka-musli@yandex.ru>
2020-10-12 07:19:47 +0300

    add adaptive

--- src/js/modules/futures-slider.js
+++ src/js/modules/futures-slider.js
@@ -1,6 +1,17 @@
 
 $('.futures-slider').slick({
+  infinite: false,
   slidesToShow: 1,
   slidesToScroll: 1,
-  dots: true
+  dots: true,
+  mobileFirst: true,
+  responsive: [
+    {
+      breakpoint: 1599,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2,
+      }
+    },
+  ]
 });
